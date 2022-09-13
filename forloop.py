4a455459050fc46de973673a4816d85c2add0ca7
Abdulla <abdulla18402@gmail.com>
2022-09-13 21:18:17 +0530

    first commit

---
+++ forloop.py
@@ -0,0 +1,12 @@
+av =5
+
+x=int(input("hey buddy how many candyes do you want..?"))
+i=1
+while i<=x :
+    if i>av:
+        print("stack ovar")
+        break
+    print("candys")
+    i=i+1
+     
+print("bye") 
---
+++ main.py
@@ -0,0 +1,10 @@
+x=1
+while x<=5:
+    print('ABDULLA ',end="")
+    j=1
+    while j<=4:
+        print("VICTOR ",end="")
+        j=j+1
+
+    x=x+1
+    print()
